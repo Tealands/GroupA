d11f63efc680db430652caeeaf9b5632f2d7c675
tokiya2525 <22.switch.switch@gmail.com>
2025-12-15 15:26:50 +0900

    共通デザイン

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare(), tailwindcss(), autoprefixer()],
+  plugins: [react(), cloudflare(), tailwindcss()],
   optimizeDeps: {
     exclude: ['@tailwindcss/oxide', 'lightningcss']
   }
